73e22bd19c7dd8dc4d73057968d6b4f283e40b73
naufan17 <muhammadnaufan719@yahoo.com>
2022-07-26 14:32:38 +0700

    revisi views font

--- src/Content.js
+++ src/Content.js
@@ -70,7 +70,7 @@ export default function Content(){
     function priceHR(prices){
         if(prices > 0){
             return(
-                <span className="relative inline-block px-2 py-1 ml-4 font-semibold text-green-900 leading-tight">
+                <span className="relative inline-block px-2 py-1 ml-2 sm:text-md text-sm font-semibold text-green-900 leading-tight">
                     <span aria-hidden="true" className="absolute inset-0 bg-green-200 opacity-50 rounded-full">
                     </span>
                     <span className="relative">
@@ -80,7 +80,7 @@ export default function Content(){
             )
         }else if(prices < 0){
             return(
-                <span className="relative inline-block px-2 py-1 ml-4 font-semibold text-red-900 leading-tight">
+                <span className="relative inline-block px-2 py-1 ml-2 sm:text-md text-sm font-semibold text-red-900 leading-tight">
                     <span aria-hidden="true" className="absolute inset-0 bg-red-200 opacity-50 rounded-full">
                     </span>
                     <span className="relative">
@@ -90,7 +90,7 @@ export default function Content(){
             )
         }else if(prices == 0){
             return(
-                <span className="relative inline-block px-2 py-1 ml-4 font-semibold text-gray-900 leading-tight">
+                <span className="relative inline-block px-2 py-1 ml-2 sm:text-md text-sm font-semibold text-gray-900 leading-tight">
                     <span aria-hidden="true" className="absolute inset-0 bg-gray-200 opacity-50 rounded-full">
                     </span>
                     <span className="relative">
@@ -104,8 +104,8 @@ export default function Content(){
     return (
         <div className="">
             <div className="flex flex-col items-center w-full mb-4 sm:flex-row">
-                <input type="search" onChange={handleSearch} placeholder="Cari nama kripto" required className="flex-grow w-full h-12 px-4 mb-3 text-gray-900 bg-transparent border-2 border-indigo-50 rounded appearance-none sm:w-full sm:mr-2 sm:mb-0 focus:border-slate-200 focus:outline-none focus:shadow-outline"/>
-                <select onChange={handleSorting} className="inline-flex w-full h-12 px-2 mb-3 font-medium text-gray-900 transition rounded shadow-sm bg-indigo-50 hover:bg-slate-200 sm:w-full sm:ml-2 sm:mb-0 focus:shadow-outline focus:outline-none">
+                <input type="search" onChange={handleSearch} placeholder="Cari nama kripto" required className="flex-grow w-full h-12 px-4 mb-3 sm:text-base text-md text-gray-900  bg-transparent border-2 border-indigo-50 rounded appearance-none sm:w-full sm:mr-2 sm:mb-0 focus:border-slate-200 focus:outline-none focus:shadow-outline"/>
+                <select onChange={handleSorting} className="inline-flex w-full h-12 px-2 mb-3 sm:text-base text-md font-medium text-gray-900 transition rounded shadow-sm bg-indigo-50 hover:bg-slate-200 sm:w-full sm:ml-2 sm:mb-0 focus:shadow-outline focus:outline-none">
                     <option value="">Urutkan</option>
                     <option value="namaAZ">Nama A-Z</option>
                     <option value="namaZA">Nama Z-A</option>
@@ -117,25 +117,25 @@ export default function Content(){
                     <option value="volumeTinggi">Volume Tinggi</option>
                 </select>
             </div>
-            <div className="grid gap-5 mb-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            <div className="grid gap-5 mb-8 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
                 {pairs.map(item => 
                 <div className="p-5 duration-300 transform bg-white border rounded shadow-sm hover:-translate-y-2">
-                    <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+                    <div className="flex items-center justify-center sm:w-12 w-10 sm:h-12 h-10 mb-4 rounded-full bg-indigo-50">
                         <img alt="profil" src={item.url_logo} className="mx-auto object-cover rounded-full h-10 w-10"/>
                     </div>
                     <div className="flex items-center justify-auto">
-                        <h6 className="font-semibold leading-5">
+                        <h6 className="sm:text-md text-sm font-semibold leading-5">
                             {item.description}
                         </h6>
                         <p>{ priceHR(((crypto[item.ticker_id].last - price[item.id]) / price[item.id] * 100).toFixed(2)) }</p>
                     </div> 
-                    <p className="text-sm font-medium text-gray-900 mb-2">
+                    <p className="sm:text-sm text-xs font-medium text-gray-900 mb-2">
                         { crypto[item.ticker_id].name }
                     </p>
-                    <p className="text-sm font-medium text-gray-900">
+                    <p className="sm:text-sm text-xs font-medium text-gray-900">
                         Harga : { (crypto[item.ticker_id].last).replace(/\B(?=(\d{3})+(?!\d))/g, ".") } IDR
                     </p>
-                    <p className="text-sm font-medium text-gray-900">
+                    <p className="sm:text-sm text-xs font-medium text-gray-900">
                         Volume : { (crypto[item.ticker_id].vol_idr).replace(/\B(?=(\d{3})+(?!\d))/g, ".") } IDR
                     </p>
                 </div>
