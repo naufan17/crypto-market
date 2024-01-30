e4bb90989982ff7b7a09f52431de9c0363033086
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-01-31 04:07:12 +0700

    add documentation

--- src/componens/Home/Loading.js
+++ src/componens/Home/Loading.js
@@ -3,17 +3,17 @@ import React from 'react';
 export default function Loading() {
     return (
         <div className="p-5 duration-300 transform bg-gradient-to-r from-indigo-50 border rounded shadow-sm shadow- hover:-translate-y-2">
-            <div class="animate-pulse">
-                <div class="flex items-center justify-center sm:w-12 w-10 sm:h-12 h-10 mb-4 rounded-full bg-slate-400">
+            <div className="animate-pulse">
+                <div className="flex items-center justify-center sm:w-12 w-10 sm:h-12 h-10 mb-4 rounded-full bg-slate-400">
                 </div>
-                <div class="space-y-4">
-                    <div class="h-3 bg-slate-400 rounded-lg">
+                <div className="space-y-4">
+                    <div className="h-3 bg-slate-400 rounded-lg">
                     </div>
-                    <div class="h-3 bg-slate-400 rounded-lg">
+                    <div className="h-3 bg-slate-400 rounded-lg">
                     </div>
-                    <div class="h-3 bg-slate-400 rounded-lg">
+                    <div className="h-3 bg-slate-400 rounded-lg">
                     </div>
-                    <div class="h-3 bg-slate-400 rounded-lg">
+                    <div className="h-3 bg-slate-400 rounded-lg">
                     </div>
                 </div>
             </div>
