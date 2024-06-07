031cd93223964a6711f42d21b1820530667210f0
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-06-07 20:50:09 +0700

    fix: view chart

--- src/componens/Market/Chart.js
+++ src/componens/Market/Chart.js
@@ -100,10 +100,8 @@ const Chart = ({ data }) => {
     }, [chartData]);
 
     return (
-        <div className="relative px-4 py-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-8">
-            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-                <div ref={chartContainerRef} style={{ width: '100%', height: '400px' }} />
-            </div> 
+        <div className="relative px-4 py-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-24">
+            <div ref={chartContainerRef} style={{ width: '100%', height: '400px' }} />
         </div>
     );
 };
