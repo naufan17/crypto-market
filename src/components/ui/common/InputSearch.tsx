6c03e142ba07bd12da890eb400147dd3b73c5957
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2024-12-27 18:27:58 +0700

    fix: padding and gap in mobile screen & feat: search icons and x in input

--- src/components/ui/common/InputSearch.tsx
+++ src/components/ui/common/InputSearch.tsx
@@ -17,7 +17,7 @@ const InputSearch: React.FC<InputSearchProps> = ({ handleSearch, placeholder }) 
         type="search" 
         onChange={handleSearch}
         placeholder={placeholder}  
-        className="inline-flex w-full sm:h-12 h-10 pl-10 pr-10 sm:text-base text-md text-gray-900 bg-transparent border-2 border-gray-400 rounded-xl appearance-none sm:w-full sm:mr-2.5 sm:mb-0 focus:border-gray-500 focus:outline-none focus:shadow-outline [&::-webkit-search-cancel-button]:appearance-none"
+        className="inline-flex w-full sm:h-12 h-10 pl-10 pr-10 sm:text-base text-md text-gray-500 bg-transparent border-2 border-gray-500 rounded-xl appearance-none sm:w-full sm:mr-2.5 sm:mb-0 focus:border-gray-700 focus:outline-none focus:shadow-outline [&::-webkit-search-cancel-button]:appearance-none"
         required
       />
       <button className="absolute right-0 px-3 sm:px-5 text-gray-500" onClick={() => handleSearch({ target: { value: '' } } as ChangeEvent<HTMLInputElement>)}>
